2515f8d15d39191c8237c9c887d7b35c64e2cb9c
Quadrubo <julweb61@gmail.com>
2022-01-18 15:18:34 +0100

    Database additions

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import sqlite3
 from sqlite3 import Error
@@ -28,6 +29,52 @@ class Database:
         except Error as e:
             print(e)
 
+    def set_daily(self, member_id):
+        sql = "UPDATE scores SET daily = ? WHERE id = ?"
+
+        conn = self.connect()
+
+        cursor = conn.cursor()
+        cursor.execute(sql, (datetime.datetime.now(), member_id))
+        conn.commit()
+
+    def collected_daily(self, member_id):
+        sql = "SELECT daily FROM scores WHERE id = ? AND CAST(daily AS DATE) = CAST( date('now') AS DATE)"
+
+        conn = self.connect()
+
+        cursor = conn.cursor()
+        cursor.execute(sql, (member_id,))
+
+        row = cursor.fetchone()
+
+        if row is None:
+            return False
+        else:
+            return True
+
+
+    def create_message(self, member_id, message_id):
+        sql = "INSERT INTO messages (id, message_id, message_datetime) VALUES (?, ?, ?)"
+
+        conn = self.connect()
+
+        cursor = conn.cursor()
+        cursor.execute(sql, (member_id, message_id, datetime.datetime.now()))
+        conn.commit()
+
+    def get_messages_from_today(self, member_id):
+        sql = "SELECT * FROM messages WHERE id = ? AND CAST(message_datetime AS DATE) = CAST( date('now') AS DATE)"
+
+        conn = self.connect()
+
+        cursor = conn.cursor()
+        cursor.execute(sql, (member_id,))
+
+        rows = cursor.fetchall()
+
+        return rows
+
     def create_member(self, member_id):
         sql = "INSERT INTO scores (id, score) VALUES (?, ?)"
 
@@ -35,6 +82,15 @@ class Database:
 
         cursor = conn.cursor()
         cursor.execute(sql, (member_id, 0))
+        conn.commit()
+
+    def set_score(self, member_id, score):
+        sql = "UPDATE scores SET score = ? WHERE id = ?"
+
+        conn = self.connect()
+
+        cursor = conn.cursor()
+        cursor.execute(sql, (score, member_id))
         conn.commit()
 
     def get_member(self, member_id):
@@ -49,6 +105,18 @@ class Database:
 
         return row
 
+    def get_scores(self):
+        sql = "SELECT * FROM scores ORDER BY score DESC"
+
+        conn = self.connect()
+
+        cursor = conn.cursor()
+        cursor.execute(sql)
+
+        rows = cursor.fetchall()
+
+        return rows
+
     def create_tables(self):
 
         sql_create_admins_table = """ 
@@ -59,7 +127,15 @@ class Database:
         sql_create_scores_table = """ 
         CREATE TABLE IF NOT EXISTS scores (
             id varchar PRIMARY KEY,
-            score int             
+            score int,
+            daily timestamp             
+        ); """
+
+        sql_create_messages_table = """
+        CREATE TABLE IF NOT EXISTS messages (
+            id varchar,
+            message_id varchar,
+            message_datetime timestamp
         ); """
 
         # create a database connection
@@ -69,4 +145,5 @@ class Database:
         if conn is not None:
             self.create_table(conn, sql_create_admins_table)
             self.create_table(conn, sql_create_scores_table)
+            self.create_table(conn, sql_create_messages_table)
 
